66bcf156684cac035b4dbc0c4dade3b91ec7b037
Apollo11-hub <Antonio-Pollo@outlook.it>
2022-05-12 18:23:19 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <TitleCompo />
+  <JumboTry />
+</div>
+</template>
+
+<script>
+
+import TitleCompo from "./components/TitleCompo.vue"
+import JumboTry from "./components/JumboTry.vue"
+
+export default {
+  name: 'App',
+  components: {
+    TitleCompo,
+    JumboTry,
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/JumboTry.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img src="Jumbotry.png" alt="Jumbotry">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JumboTry"
+}
+</script>
+
+<style>
+
+</style>
